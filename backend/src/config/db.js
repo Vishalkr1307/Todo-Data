58a33f52fccae5212877e58fd33512fe6fd21ca0
Vishal Kumar <vishal.kuamr459@gmail.com>
2023-12-02 03:08:38 +0530

    makeServer

---
+++ backend/src/config/db.js
@@ -0,0 +1,7 @@
+const mongoose=require("mongoose")
+
+require("dotenv").config()
+
+module.exports=()=>{
+    return mongoose.connect(process.env.DB).then(()=>console.log("Connect success to database")).catch(()=>console.log("connect failure"))
+}
